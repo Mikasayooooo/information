622284830bbb2a7f754e489bd3a60d626207c838
lhl <Author Email>
2020-10-27 22:51:13 +0800

    图片验证码去重完善

--- info/modules/passport/views.py
+++ info/modules/passport/views.py
@@ -2,11 +2,18 @@ from . import passport_blue
 from info.utils.captcha.captcha import captcha
 # 注意导包路径
 
+from flask import request, current_app, make_response
+from info import constants
+
 from captcha.image import ImageCaptcha
 from random import randint
 
 
 # 功能: 获取图片验证码
+# 请求路径: /passport/image_code
+# 请求方式: GET
+# 请求参数: cur_id,pre_id
+# 返回值: 图片验证码
 @passport_blue.route('/image_code')
 def image_code():
     # list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
@@ -21,10 +28,33 @@ def image_code():
     # image_data = ImageCaptcha().generate_image(chars)
     # image_data.show()
 
+    # 获取前端传递的参数
+    cur_id = request.args.get('cur_id')
+    pre_id = request.args.get('pre_id')
+
     # 调用generate_captcha 获取图片验证码编号,验证码值,图片(二进制)
     name, text, image_data = captcha.generate_captcha()
 
+    from info import redis_store
+    # 采用局部导入的方式,因为redis_store声明了全局变量为None,redis_store = None,
+    # 在上面导入的话,此时redis_store还只是个none值,需要等执行create_app函数之后才有值
+
+    # 将图片验证码的值保存在redis中
+    # 参数1: key,参数2: value,参数3: 有效期
+    redis_store.set('image_store: {}'.format(cur_id), text, constants.IMAGE_CODE_REDIS_EXPIRES)
+
+    # 判断是否有上一次的图片验证码
+    try:
+        # 判断是否有上一次的图片验证码
+        if pre_id:
+            redis_store.delete('image_store: {}'.format(pre_id))
+    except Exception as e:
+        current_app.logger.error(e)
+        return '图片验证码操作失败'
+
     # 返回图片
-    return image_data
+    response = make_response(image_data)
+    response.headers['Content-Type'] = 'image/png'
+    return response
 
 # image_code()
