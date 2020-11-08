a93e16cdb802a63338396cef39bda14e20232c34
lhl <Author Email>
2020-11-08 22:11:37 +0800

    用户的密码修改

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -6,11 +6,55 @@ from info.utils.commons import user_login_data
 
 
 
+# 获取/设置用户密码
+# 请求路径: /user/pass_info
+# 请求方式: GET,POST
+# 请求参数: GET无，POST请求有参数，old_password,new_password
+# 返回值: GET请求：user_pass_info.html页面， POST请求： errno,errmsg
+@profile_blue.route('/pass_info', methods=['GET', 'POST'])
+@user_login_data
+def pass_info():
+    '''
+    1.判断请求方式，如果是get请求
+    2.直接渲染页面
+    3.如果是post请求，获取参数
+    4.校验参数，为空校验
+    5.判断老密码是否正确
+    6.设置新密码
+    7.返回响应
+    :return:
+    '''
+
+    # 1.判断请求方式，如果是get请求
+    if request.method == 'GET':
+        # 2.直接渲染页面
+        return render_template('news/user_pass_info.html')
+
+    # 3.如果是post请求，获取参数
+    old_password = request.json.get('old_password')
+    new_password = request.json.get('new_password')
+
+    # 4.校验参数，为空校验
+    if not all([old_password,new_password]):
+        return jsonify(errno=RET.PARAMERR, errmsg='参数不全')
+
+    # 5.判断老密码是否正确
+    if not g.user.check_password(old_password):
+        return jsonify(errno=RET.DATAERR, errmsg='老密码错误')
+
+    # 7.设置新密码
+    g.user.password = new_password
+
+    # 8.返回响应
+    return jsonify(errno=RET.OK, errmsg='修改成功')
+
+
+
 # 获取/设置用户头像上传
 # 请求路径: /user/pic_info
 # 请求方式: GET,POST
-# 请求参数: POST请求有参数，avatar
-# 返回值: GET请求：user_info_pic.html页面，data字典数据 ， POST请求： errno,errmsg，avatar_url
+# 请求参数: GET无，POST请求有参数，avatar
+# 返回值: GET请求：user_pic_info.html页面，data字典数据 ， POST请求： errno,errmsg，avatar_url
 @profile_blue.route('/pic_info', methods=['GET', 'POST'])
 @user_login_data
 def pic_info():
@@ -28,9 +72,9 @@ def pic_info():
 
     # 1.判断请求方式，如果是get请求
     if request.method == 'GET':
-        return render_template('news/user_pic_info.html',user_info=g.user.to_dict())
+        # 2.携带用户数据，渲染页面
+        return render_template('news/user_pic_info.html', user_info=g.user.to_dict())
 
-    # 2.携带用户数据，渲染页面
     # 3.如果是post请求
     # 4.获取参数
     # 5.校验参数，为空校验
