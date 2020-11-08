1ec7e17079402c0ce6b4be32c37f3753e2c01921
lhl <Author Email>
2020-11-08 15:33:10 +0800

    用户中心右上角数据展示

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,7 +1,18 @@
-from flask import render_template
+from flask import render_template, g,redirect
 from  . import profile_blue
+from info.utils.commons import user_login_data
 
-@profile_blue.route('/user_index')
+
+@profile_blue.route('/info')
+@user_login_data
 def user_index():
-    data = {}
+
+    # 1. 判断用户是否登录
+    if not g.user:
+        return redirect('/')
+
+    # 2. 携带数据渲染页面
+    data = {
+        'user_info':g.user.to_dict()
+    }
     return render_template('news/user.html',data=data)
