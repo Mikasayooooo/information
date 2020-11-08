aeadfa19deb67db310d13e2a0a9e6a82b5265770
lhl <Author Email>
2020-11-08 14:29:56 +0800

    点赞 & 取消点赞后端完成

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,10 +1,101 @@
-from flask import current_app, jsonify, render_template, abort, session, g,request
+from flask import current_app, jsonify, render_template, abort, session, g, request
 
 from info import db
-from info.models import News, User,Comment,CommentLike
+from info.models import News, User, Comment, CommentLike
 from info.utils.commons import user_login_data
 from info.utils.response_code import RET
 from . import news_blue
+
+
+# 评论点赞
+# 请求路径: /news/comment_like
+# 请求方式: POST
+# 请求参数: news_id(可以不传),comment_id,g.user,action
+# 返回值: errno,errmsg
+@news_blue.route('/comment_like', methods=['POST'])
+@user_login_data
+def comment_like():
+    '''
+    1.判断用户是否登陆
+    2.获取请求参数
+    3.校验参数,为空校验
+    4.操作类型进行校验
+    5.通过评论编号查询评论对象，并判断是否存在
+    6.根据操作类型点赞取消点赞
+    7.返回响应
+    :return:
+    '''
+
+    # 1.判断用户是否登陆
+    if not g.user:
+        return jsonify(errno=RET.NODATA, errmsg='用户未登录')
+
+    # 2.获取请求参数
+    comment_id = request.json.get('comment_id')
+    action = request.json.get('action')
+
+    # 3.校验参数,为空校验
+    if not all([comment_id, action]):
+        return jsonify(errno=RET.PARAMERR, errmsg='参数不全')
+
+    # 4.操作类型进行校验
+    if not action in ['add', 'remove']:
+        return jsonify(errno=RET.DATAERR, errmsg='操作类型 有误')
+
+    # 5.通过评论编号查询评论对象，并判断是否存在
+    try:
+        # comment = Comment.query.filter(Comment.id == comment_id)  错误
+        comment = Comment.query.get(comment_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg='评论获取失败')
+
+    if not comment:
+        return jsonify(errno=RET.NODATA, errmsg='评论不存在')
+
+    # 6.根据操作类型点赞取消点赞
+    try:
+
+        if action == 'add':
+            # 6.1 判断用户是否有对该评论点过赞
+            comment_like = CommentLike.query.filter(CommentLike.user_id == g.user.id,
+                                                    CommentLike.comment_id == comment_id).first()
+            if not comment_like:
+                # 创建点赞对象
+                comment_like = CommentLike()
+                comment_like.user_id = g.user.id
+                comment_like.comment_id = comment_id
+
+                # 添加到数据库中
+                db.session.add(comment_like)
+
+                # 将该评论数量 加1
+                comment.like_count += 1
+
+                # 数据库自动提交有一定的延时性，加入用户暴力点击，就会出现问题
+                db.session.commit()
+
+        else:
+            # 6.1 判断用户是否有对该评论点过赞
+            comment_like = CommentLike.query.filter(CommentLike.user_id == g.user.id,
+                                                    CommentLike.comment_id == comment_id).first()
+            if comment_like:
+                # 删除点赞对象
+                db.session.delete(comment_like)
+
+                # 将该评论数量 减1
+                if comment.like_count > 0:
+                    comment.like_count -= 1
+
+                # 数据库自动提交有一定的延时性，加入用户暴力点击，就会出现问题
+                db.session.commit()
+
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg='操作失败')
+
+    # 7.返回响应
+    return jsonify(errno=RET.OK, errmsg='操作成功')
 
 
 # 新闻评论后端
@@ -12,7 +103,7 @@ from . import news_blue
 # 请求方式: POST
 # 请求参数: news_id,comment,g.user,parent_id
 # 返回值: errno,errmsg,评论字典
-@news_blue.route('/news_comment',methods=['POST'])
+@news_blue.route('/news_comment', methods=['POST'])
 @user_login_data
 def news_comment():
     '''
@@ -28,7 +119,7 @@ def news_comment():
 
     # 1.判断用户是否登陆
     if not g.user:
-        return jsonify(errno=RET.NODATA,errmsg='用户未登录')
+        return jsonify(errno=RET.NODATA, errmsg='用户未登录')
 
     # 2.获取请求参数
     news_id = request.json.get('news_id')
@@ -36,12 +127,12 @@ def news_comment():
     parent_id = request.json.get('parent_id')
 
     # 3.校验参数,为空校验
-    if not all([news_id,content]):
+    if not all([news_id, content]):
         return jsonify(errno=RET.PARAMERR, errmsg='参数不全')
 
     # 4.根据新闻编号取出新闻对象,判断新闻是否存在
     try:
-       news = News.query.get(news_id)
+        news = News.query.get(news_id)
     except Exception as e:
         current_app.logger.error(e)
         return jsonify(errno=RET.DBERR, errmsg='新闻获取失败')
@@ -60,15 +151,15 @@ def news_comment():
 
     # 6.保存评论对象到数据库中
     try:
-       db.session.add(comment)
-       db.session.commit()
-       #因为前面已经设置过了数据库自动提交,所以这句话可以不写
+        db.session.add(comment)
+        db.session.commit()
+        # 因为前面已经设置过了数据库自动提交,所以这句话可以不写
     except Exception as e:
         current_app.logger.error(e)
         return jsonify(errno=RET.DBERR, errmsg='评论失败')
 
     # 7.返回响应,携带评论的数据
-    return jsonify(errno=RET.OK, errmsg='评论成功',data=comment.to_dict())
+    return jsonify(errno=RET.OK, errmsg='评论成功', data=comment.to_dict())
 
 
 # 收藏功能接口
@@ -76,7 +167,7 @@ def news_comment():
 # 请求方式: POST
 # 请求参数: news_id,action,g.user
 # 返回值: errno,errmsg
-@news_blue.route('/news_collect',methods=['POST'])
+@news_blue.route('/news_collect', methods=['POST'])
 @user_login_data
 def news_collect():
     '''
@@ -93,23 +184,23 @@ def news_collect():
 
     # 1.判断用户是否登录
     if not g.user:
-        return jsonify(errno=RET.NODATA,errmsg='用户未登录')
+        return jsonify(errno=RET.NODATA, errmsg='用户未登录')
 
     # 2.获取参数
     news_id = request.json.get('news_id')
     action = request.json.get('action')
 
     # 3.参数校验,为空校验
-    if not all([news_id,action]):
+    if not all([news_id, action]):
         return jsonify(errno=RET.PARAMERR, errmsg='参数不全')
 
     # 4.操作类型校验
-    if not action in ['collect','cancel_collect']:
+    if not action in ['collect', 'cancel_collect']:
         return jsonify(errno=RET.DATAERR, errmsg='操作类型有误')
 
     # 5.根据新闻的编号取出新闻对象
     try:
-       news = News.query.get(news_id)
+        news = News.query.get(news_id)
     except Exception as e:
         current_app.logger.error(e)
         return jsonify(errno=RET.DBERR, errmsg='新闻获取失败')
@@ -148,7 +239,6 @@ def news_detail(news_id):
     # except Exception as e:
     #     current_app.logger.error(e)
 
-
     # 1.根据新闻编号,查询新闻对象
     try:
         news = News.query.get(news_id)
@@ -175,7 +265,6 @@ def news_detail(news_id):
     # for news in click_news_list:
     #     click_news_list.append(news.to_dict())
 
-
     # 5.判断用户是否收藏过该新闻
     is_collected = False
     # 用户需要登陆,并且该新闻在用户收藏过的新闻列表中
@@ -183,10 +272,9 @@ def news_detail(news_id):
         if news in g.user.collection_news:
             is_collected = True
 
-
     # 6.查询数据库中,该新闻的所有评论内容
     try:
-       comments = Comment.query.filter(Comment.news_id == news_id).order_by(Comment.create_time.desc())
+        comments = Comment.query.filter(Comment.news_id == news_id).order_by(Comment.create_time.desc())
     except Exception as e:
         current_app.logger.error(e)
         return jsonify(errno=RET.DBERR, errmsg='获取评论失败')
@@ -207,14 +295,13 @@ def news_detail(news_id):
 
     except Exception as e:
         current_app.logger.error(e)
-        return jsonify(errno=RET.DBERR,errmsg='获取点赞失败')
-
+        return jsonify(errno=RET.DBERR, errmsg='获取点赞失败')
 
     # 7.将评论对象列表转换成字典列表
     # comments_list = [comment.to_dict() for comment in comments]
     comments_list = []
     for comment in comments:
-        #将评论对象,转字典
+        # 将评论对象,转字典
         comm_dict = comment.to_dict()
 
         # 添加is_like记录点赞
@@ -222,11 +309,10 @@ def news_detail(news_id):
 
         # 判断用户是否有对评论点过赞
         # 首先判断用户是否存在，然后在判断 评论编号 是否 在 用户点赞过的评论列表里
-        if g.user and comment.id in  mylike_comment_ids:
+        if g.user and comment.id in mylike_comment_ids:
             comm_dict['is_like'] = True
 
         comments_list.append(comm_dict)
-
 
     # 8.携带数据,渲染页面
     data = {
@@ -237,8 +323,8 @@ def news_detail(news_id):
         'user_info': g.user.to_dict() if g.user else '',
         'news': click_news_list,
         #     user_info  和 news  必须和 base.html一一对应
-        'is_collected':is_collected,
-        'comments_list':comments_list
+        'is_collected': is_collected,
+        'comments_list': comments_list
     }
 
     return render_template('news/detail.html', data=data)
