4983fa9f83b0cb56a134db9a2b839ede04a4037c
lhl <Author Email>
2020-11-12 17:02:05 +0800

    防止普通用户访问管理员的其他页面

--- info/modules/admin/__init__.py
+++ info/modules/admin/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request, session, redirect
 
 # 创建蓝图
 admin_blue = Blueprint('admin', __name__,url_prefix='/admin')
@@ -6,3 +6,27 @@ admin_blue = Blueprint('admin', __name__,url_prefix='/admin')
 # 导入views文件装饰视图函数
 # from info.module.index import views
 from . import views
+
+
+# 使用请求钩子，拦截用户的请求，只有访问了admin_blue,所装饰的视图函数需要拦截
+# 1.拦截的是访问了非登陆页面
+# 2.拦截的是普通用户
+
+@admin_blue.before_request
+def before_request():
+    # print(request.url)
+    # 1.拦截的是访问了非登陆页面
+    # if request.url.endswith('/admin/login'):
+    #     pass
+    # else:
+    #     # 2.判断是否是管理员
+    #     if session.get('is_admin'):
+    #         pass
+    #     else:
+    #         return redirect('/')
+
+    # 改装上面的代码
+    if not request.url.endswith('/admin/login'):
+        if not session.get('is_admin'):
+            return redirect('/')
+
